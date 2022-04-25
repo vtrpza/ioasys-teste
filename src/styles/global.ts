5614ba73889ac35fcf7b55273811ecccfbc644b1
Vitor Pouza <vhnpouza@gmail.com>
2022-04-25 20:25:33 -0300

    theme and font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,27 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: 'Heebo';
+  font-style: normal;
+  font-weight: 400;
+  src: local(''),
+       url('../fonts/heebo-v19-latin-regular.woff2') format('woff2'), 
+}
+@font-face {
+  font-family: 'Heebo';
+  font-style: normal;
+  font-weight: 500;
+  src: local(''),
+       url('../fonts/heebo-v19-latin-500.woff2') format('woff2'),
+}
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
 }
 
 html {
@@ -16,7 +33,7 @@ html, body, #__next {
 }
 
 body {
-    font-family: ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'Heebo',---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
 }`
 
 export default GlobalStyles
